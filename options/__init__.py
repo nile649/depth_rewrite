8db0446bd2a8e7f242d6579621a994cbca0d5742
nile649 <nile649@gmail.com>
2019-12-20 13:43:33 -0500

    add options

--- options/__init__.py
+++ options/__init__.py
@@ -1,10 +1,12 @@
 from .train import _get_train_opt
-# from .test import _get_test_opt
+from .test import _get_test_opt
 
 def get_args(mode):
     args = None
     if mode == 'train':
         args =  _get_train_opt()
+    elif mode == 'test':
+    	args = _get_test_opt()
     else:
         raise ValueError("Invalid mode selection!")
 
