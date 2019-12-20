a080e6400dc3844600d2afa20c558866fa93bcc8
nile649 <nile649@gmail.com>
2019-12-19 22:25:12 -0500

    message new files made changes

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from data import data_loader
 from model.network import Depth_SARPN
 from options import get_args
-
+from model.util import *
 
 
 args = get_args('train')
@@ -17,19 +17,30 @@ print(args.root_path)
 TrainImgLoader = data_loader.getTrainingData_NYUDV2(args.batch_size, args.root_path+args.trainlist_path, args.root_path+'/data/')
 depth_model = Depth_SARPN(args)
 
+# Evaluate
+TestImgLoader = data_loader.getTestingData_NYUDV2(1, args.root_path+args.trainlist_path, args.root_path+'/data/')
 
 
 def train():
 	print('learning rate ----> {}'.format(depth_model.get_lr()))
 	for epoch in range(0,args.epochs):
+		mode = "train"
 		for idx, sample in enumerate(TrainImgLoader):
 			depth_model.setInput(sample)
 			depth_model.optimize_parameters()
-			depth_model.print_loss(args.epochs,epoch,TrainImgLoader)
 
-		if epoch%args.save_itr==0:
-			depth_model.save_network(epoch)
-			depth_model.save_checkpoint(epoch)
+		depth_model.print_loss(args.epochs,epoch,TrainImgLoader,mode)
+		evaluate(epoch)
+
+def evaluate(epoch):
+	for idx, sample in enumerate(TestImgLoader):
+	        depth_model.setInput(sample)
+	        depth_model.evaluate()
+	depth_model.print_test()
+
+
+
+
 
 if __name__ == '__main__':
 	train()
